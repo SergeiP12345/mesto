ebf74098da95c93dc55f3c649b42c14265044b61
SergeiP12345 <115816302+SergeiP12345@users.noreply.github.com>
2022-12-14 17:18:18 +0300

    проверка ревью5

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 const aboutButton = document.querySelector ('.profile__button');
 const aboutPopup = document.querySelector ('.popup');
 const aboutButtonclose = document.querySelector ('.popup__close-button');
-let UserName = document.querySelector ('.profile__title');
-let UserText = document.querySelector ('.profile__text');
+let userName = document.querySelector ('.profile__title');
+let userText = document.querySelector ('.profile__text');
 let formElement = document.querySelector ('.popup__form');
-let NameInput = document.querySelector ('.popup__name');
-let textInput = document.querySelector ('.popup__text');
-let popupSumbit = document.querySelector ('.popup__save-button');
+let nameInput = document.querySelector ('.popup__name');
+let textInput = document.querySelector ('.popup__name_text');
+
 
 
 
   aboutButton.addEventListener  ('click' , (evt) => { 
-    evt.preventDefault ();
+    
      aboutPopup.classList.add('popup_opened');
-     NameInput.value = UserName.textContent;
-     textInput.value = UserText.textContent;
+     nameInput.value = userName.textContent;
+     textInput.value = userText.textContent;
  });
 
  aboutButtonclose.addEventListener  ('click' , () => {
@@ -24,8 +24,8 @@ let popupSumbit = document.querySelector ('.popup__save-button');
  
  function handleFormSubmit (evtm) {
      evtm.preventDefault();
-     UserName.textContent = NameInput.value;
-    UserText.textContent = textInput.value;
+     userName.textContent = nameInput.value;
+    userText.textContent = textInput.value;
     aboutPopup.classList.remove('popup_opened');
    
  }
